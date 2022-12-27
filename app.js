2567b674b2fc467554dcb85c9586174077f64914
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2022-12-27 16:49:59 -0300

    add config cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const routePets = require("./routes/routePets");
 const bodyParser = require("body-parser");
 
+app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
